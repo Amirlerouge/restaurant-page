e6a13a665d6776c0c2e0d0dbf55cf1390f397ca0
Amir <amirbooboo@gmail.com>
2021-10-19 14:05:03 +0200

    new look

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import './style.css';
 
 
-alert("this works");
 
+
+    window.addEventListener('DOMContentLoaded', (event) => {
+        
+        let sidemenu = document.getElementsByClassName("glass");
+
+        sidemenu.onclick = function(event) {
+            let target = event.target; // where was the click?
+          
+            if (target.tagName == 'link1'){
+                alert("target"); // highlight it
+            } return; // not on TD? Then we're not interested
+          
+            alert(target.tagName); // highlight it
+
+          };
+
+
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,18 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+
+      {
+
+        test: /\.html$/i,
+      use: [
+        {
+          loader: 'html-loader'
+        }]
+      },
+      
+      
     ],
+    
   },
 };
